b00e076740cfe4aed19f2b9b565b488ae3322911
chanmi24 <chanmi24@gmail.com>
2026-03-11 15:26:04 +0900

    savecreate

--- src/services/lectureService.js
+++ src/services/lectureService.js
@@ -3,6 +3,12 @@ import axios from './httpRequester';
 const path = '/lectures'; // 프록시 설정을 위해 /api를 붙이는 것이 안전합니다.
 
 export class LectureService {
+
+    // 강의 생성
+        static async postLecture(data) {
+        const res = await axios.post(`${path}/create`, data);
+        return res.data.result;
+    }
     
     // 건물 목록 조회
     static async getBuildings() {

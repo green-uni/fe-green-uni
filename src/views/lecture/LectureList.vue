81e1ad9c711694549dee091f937b03720c6a5429
qwazsx1346 <qwazsx1346@gmail.com>
2026-03-12 10:06:19 +0900

    lectureList파일 생성

---
+++ src/views/lecture/LectureList.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+<div><button>코딩학개론</button></div>
+<div><button>사랑과전쟁</button></div>
+<div><button>스포츠역학</button></div>
+<div><button>인체해부학</button></div>
+</template>
+
+<style scoped>
+
+</style>

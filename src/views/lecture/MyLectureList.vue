d3878e33ee3fb36eed413bedebeb5d8e78f199ea
qwazsx1346 <qwazsx1346@gmail.com>
2026-03-12 14:32:21 +0900

    출석관리 작업중

--- src/views/lecture/MyLectureList.vue
+++ src/views/lecture/MyLectureList.vue
@@ -1,12 +1,24 @@
 <script setup>
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+onMounted(()=> {
+
+});
+
+const coding = () => {
+    router.push('/lectures/me/:lectureId')
+};
 
 </script>
 
 <template>
-<div><button>코딩학개론</button></div>
-<div><button>사랑과전쟁</button></div>
-<div><button>스포츠역학</button></div>
-<div><button>인체해부학</button></div>
+<div><button @click="coding">코딩학개론</button></div>
+<div><button @click="love">사랑과전쟁</button></div>
+<div><button @click="sports">스포츠역학</button></div>
+<div><button @click="inc">인체해부학</button></div>
 </template>
 
 <style scoped>

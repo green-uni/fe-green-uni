718d44eba57e657485b56f06ef7d11539db74dac
junlee <myyouthhada@naver.com>
2026-03-16 12:46:03 +0900

    버튼 컴포넌트화

--- src/utils/button.js
+++ src/utils/button.js
@@ -1,4 +1,10 @@
-// 임시저장 버튼
+/*
+임시저장, 저장, 취소 버튼
+사용법
+- import { saveToLocalStorage, loadfromLocalStorage, clearLocalStorage, DRAFT_KEY } from '@/utils/button';
+*/
+
+export const DRAFT_KEY = 'majorCreateDraft';
 
 export const saveToLocalStorage = (key, data) => {
   localStorage.setItem(key, JSON.stringify(data))

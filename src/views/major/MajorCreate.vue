54412e7cb72f477a5c20b9c4bd9296723c63d163
junlee <myyouthhada@naver.com>
2026-03-17 16:18:10 +0900

    학과장id가 db에 저장되게 하기

--- src/views/major/MajorCreate.vue
+++ src/views/major/MajorCreate.vue
@@ -2,35 +2,39 @@
 import CalendarDate from '@/components/util/CalendarDate.vue';
 import majorService from '@/services/majorService';
 import { saveToLocalStorage, loadfromLocalStorage, clearLocalStorage, DRAFT_KEY } from '@/utils/button';
-import { reactive, onMounted, computed } from 'vue';
+import { reactive, onMounted, computed, ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { useModalStore } from '@/stores/modal';
+import SearchInput from '@/components/util/SearchInput.vue';
 
 const route = useRoute();
 const router = useRouter();
 const modal = useModalStore();
 
 const state = reactive({
+  professorList: [],
   name: '',
   active: 'running', //정상 = running, 폐지 = closed
   college: '',
   room: '',
   tel: '',
-  chairProfessor: '',
+  chairProfessorId: '',
   capacity: '',
   startDate: '',
   info: ''
 });
 
+const profSearchName = ref('');
 const colleges = ['인문대학', '자연대학', '공과대학', '예술대학', '교양학부'];
 
 // 초기화
 function reset() {
   Object.assign(state, {
     name: '', active: 'running', college: '',
-    room: '', tel: '', chairProfessor: '',
+    room: '', tel: '', chairProfessorId: '',
     capacity: '', startDate: '', info: '',
   });
+  profSearchName.value = '';
 }
 
 // 유효성 검사 함수
@@ -38,7 +42,7 @@ function validate() {
   const requiredFields = {
     name: '학과명',
     college: '소속대학',
-    chairProfessor: '학과장명',
+    chairProfessorId: '학과장명',
     active: '학과 상태',
     room: '학과사무실',
     capacity: '입학정원',
@@ -101,11 +105,34 @@ function save() {
 // 수정 모드 여부
 const isEdit = computed(() => !!route.params.majorId); //!!: 값을 boolean으로 강제 변환하는 표현 => "majorId가 존재하면 true, 없으면 false" 를 깔끔하게 표현
 
+// 교수 목록 가져오기 함수 추가
+// 비록 목록을 처음부터 화면에 펼쳐서 보여주지는 않더라도, 사용자가 입력창에 글자를 쳤을 때 "어떤 데이터 중에서 검색할지"에 대한 기준 데이터가 프론트엔드 메모리에 미리 올라와 있어야 함
+async function fetchProfessors() {
+  try {
+    const res = await majorService.getProfessorList();
+    state.professorList = res.result;
+  } catch (e) {
+    console.error('교수 목록 조회 실패', e);
+  }
+}
+
+// 교수 선택 시 실행될 함수
+function handleProfessorSelect(professor) {
+  profSearchName.value = professor.name;
+  state.chairProfessorId = professor.memberId;
+}
+
 onMounted(async () => {
+  await fetchProfessors();
+
   if (isEdit.value) {
     try {
       const res = await majorService.getMajor(route.params.majorId);
-      Object.assign(state, res.result ?? res);
+      const data = res.result ?? res;
+      Object.assign(state, data);
+      // 수정 페이지 진입 시 학과장 이름을 검색창에 띄워줌
+      profSearchName.value = data.chairProfessorName;
+    
     } catch (e) {
       console.error('학과 정보 조회 실패', e);
     }
@@ -150,9 +177,8 @@ const pageTitle = computed(() => isEdit.value ? '학과 정보 수정' : '학과
           <div class="input-wrap">
             <div class="input-label">학과장명</div>
             <div class="input-content">
-              <label>
-                <input v-model="state.chairProfessor" type="text" class="input-box narrow" />
-              </label>
+              <SearchInput v-model="profSearchName" :list="state.professorList" placeholder="교수명을 입력하세요"
+                @select="handleProfessorSelect" />
             </div>
           </div>
           <div class="input-wrap">
